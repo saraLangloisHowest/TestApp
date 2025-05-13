8b8fc4963a5d7ee43839393244336eeb6f6d1fac
Sara Langlois <sara.langlois@student.howest.be>
2025-05-13 06:53:42 +0200

    Add json file

---
+++ WebApplication1/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Core.Entities;
+using WebApplication1.Data;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public ApplicationUsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _dbContext.ApplicationUsers.Select(u=>new BaseViewModel { Id = u.Id.ToString(), Name = u.UserName }).ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
